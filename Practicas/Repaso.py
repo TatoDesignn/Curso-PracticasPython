f2df172563600fd27aa36011abe4c8b19df38795
TATO DESIGN <73967293+TatoDesign@users.noreply.github.com>
2023-03-06 01:08:52 -0500

    Repaso actua

--- Practicas/Repaso.py
+++ Practicas/Repaso.py
@@ -1,23 +1,7 @@
-employees = [
-    ("Rolf Smith", 35, 8.75),
-    ("Anne Pun", 30, 12.50),
-    ("Charlie Lee", 50, 15.50),
-    ("Bob Smith", 20, 7.00)
-]
+conjunto = set()
 
-total2 = 0
+conjunto.add("primera")
+conjunto.add("segunda")
+conjunto.add("tercera")
 
-for i in employees:
-    total = i[1] * i[2]
-    total2 = total2 + i[2]
-    print(f"{i[0]} se le debe pagar: {total}")
-
-promedio = total2 / len(employees)
-
-for k in employees:
-    if(k[2] > promedio):
-        print(f"{k[0]} gana mas del promedio")
-
-
-
-
+print(conjunto)
